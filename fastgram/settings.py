750fed67c7eaebfae648725c707cb97f87834bfa
MilKMS <84441925+MilKMS@users.noreply.github.com>
2021-05-31 19:42:30 +0900

    Update settings.py

--- fastgram/settings.py
+++ fastgram/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!#589*w7(u=cf326p9sffulx^$j+&l+3*b$0fks_z8$u4i@21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
